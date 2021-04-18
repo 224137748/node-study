8d238332206e59b53e254976ccd4e6216dfe829f
ZHUbinxin0207 <snoopy_zuozuo@qq.com>
2021-04-19 01:31:07 +0800

    22

--- node/api/vue-auto-router-cli/lib/download.js
+++ node/api/vue-auto-router-cli/lib/download.js
@@ -2,8 +2,8 @@ const { promisify } = require("util");
 const download = require("download-git-repo");
 // 下载的动画
 const ora = require("ora");
-const clone = async (repo, target) => {};
-const process = ora(`下载...${repo}`);
-
+const clone = async (repo, target) => {
+  const process = ora(`下载...${repo}`);
+};
 
 module.exports = clone;
